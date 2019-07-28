fd9d40078f6407ac56720a6283241a9c0fb20ec9
phatcharaphan <phat.pan@gmail.com>
2019-07-28 22:52:32 +0700

    create get blog list

---
+++ blog/blog.go
@@ -0,0 +1,35 @@
+package blog
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type response struct {
+	result []blog `json:"result"`
+}
+
+type blog struct {
+	Name     string `json:"name"`
+	Content  string `json:"errorMsg"`
+	Clapping int    `json:"clapping"`
+}
+
+func GetBlogList(c echo.Context) error {
+	response := response{
+		result: []blog{
+			blog{
+				Name:     "On The Subject Of Subjects (in RxJS)",
+				Content:  "Subjects in RxJS are often misunderstood. Because they allow you to imperatively push values into an observable stream, people tend to abuse Subjects when they’re not quite sure how to make an Observable out of something. The pattern looks a little like this",
+				Clapping: 1,
+			},
+			blog{
+				Name:     "On The Subject Of Subjects (in RxJS)",
+				Content:  "Subjects in RxJS are often misunderstood. Because they allow you to imperatively push values into an observable stream, people tend to abuse Subjects when they’re not quite sure how to make an Observable out of something. The pattern looks a little like this",
+				Clapping: 1,
+			},
+		},
+	}
+	return c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	blog "github.com/phatpan/blog-example-api/blog"
 )
 
 func main() {
@@ -11,5 +12,6 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/blog", blog.GetBlogList)
 	e.Logger.Fatal(e.Start(":1323"))
 }
