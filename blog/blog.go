35341bdb22b95053ba14a6247bf81e2c0983396b
phatcharaphan <phat.pan@gmail.com>
2019-07-28 22:53:11 +0700

    create get blog list

--- blog/blog.go
+++ blog/blog.go
@@ -11,6 +11,7 @@ type response struct {
 }
 
 type blog struct {
+	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Content  string `json:"errorMsg"`
 	Clapping int    `json:"clapping"`
@@ -20,11 +21,13 @@ func GetBlogList(c echo.Context) error {
 	response := response{
 		result: []blog{
 			blog{
+				ID:       1,
 				Name:     "On The Subject Of Subjects (in RxJS)",
 				Content:  "Subjects in RxJS are often misunderstood. Because they allow you to imperatively push values into an observable stream, people tend to abuse Subjects when they’re not quite sure how to make an Observable out of something. The pattern looks a little like this",
 				Clapping: 1,
 			},
 			blog{
+				ID:       2,
 				Name:     "On The Subject Of Subjects (in RxJS)",
 				Content:  "Subjects in RxJS are often misunderstood. Because they allow you to imperatively push values into an observable stream, people tend to abuse Subjects when they’re not quite sure how to make an Observable out of something. The pattern looks a little like this",
 				Clapping: 1,
